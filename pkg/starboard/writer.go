e8b411be364513fe86969a0bae8e8fa65dea627d
Daniel Pacak <pacak.daniel@gmail.com>
2020-04-19 18:53:20 +0200

    feat: Create or update vulnerabilities report
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- pkg/starboard/writer.go
+++ pkg/starboard/writer.go
@@ -4,6 +4,8 @@ import (
 	sec "github.com/aquasecurity/k8s-security-crds/pkg/apis/aquasecurity/v1alpha1"
 	clientset "github.com/aquasecurity/k8s-security-crds/pkg/generated/clientset/versioned"
 	"github.com/aquasecurity/starboard-aqua-csp-webhook/pkg/etc"
+	log "github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/api/errors"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -24,11 +26,25 @@ func NewWriter(config etc.Starboard, client clientset.Interface) Writer {
 }
 
 func (s *writer) Write(name string, report sec.VulnerabilityReport) (err error) {
-	_, err = s.client.AquasecurityV1alpha1().Vulnerabilities(s.config.Namespace).Create(&sec.Vulnerability{
-		ObjectMeta: meta.ObjectMeta{
-			Name: name,
-		},
-		Report: report,
-	})
+	vulnerability, err := s.client.AquasecurityV1alpha1().Vulnerabilities(s.config.Namespace).Get(name, meta.GetOptions{})
+	if err != nil && errors.IsNotFound(err) {
+		log.WithField("name", name).Debug("Creating vulnerabilities report")
+		_, err = s.client.AquasecurityV1alpha1().Vulnerabilities(s.config.Namespace).Create(&sec.Vulnerability{
+			ObjectMeta: meta.ObjectMeta{
+				Name: name,
+			},
+			Report: report,
+		})
+		return
+	}
+	if err != nil {
+		return
+	}
+	copied := vulnerability.DeepCopy()
+	copied.Report = report
+
+	log.WithField("name", name).Debug("Updating vulnerabilities report")
+	_, err = s.client.AquasecurityV1alpha1().Vulnerabilities(s.config.Namespace).Update(copied)
+
 	return
 }
